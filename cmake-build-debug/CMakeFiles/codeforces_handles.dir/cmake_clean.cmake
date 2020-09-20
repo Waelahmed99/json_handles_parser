e2e1709b6883ce13b522a8950effffb566cce237
Waelahmed99 <Waelahmed6599@gmail.com>
2020-09-20 20:29:27 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/codeforces_handles.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codeforces_handles.dir/main.cpp.o"
+  "codeforces_handles"
+  "codeforces_handles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codeforces_handles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define NAMES "/home/wael/CLionProjects/codeforces_handles/names.txt"
+#define HANDLES "/home/wael/CLionProjects/codeforces_handles/handles.txt"
+#define OUTPUT "/home/wael/CLionProjects/codeforces_handles/handles.json"
+
+using namespace std;
+
+vector<string> readData(const string& fileName) {
+    ifstream file(fileName);
+    string line;
+    vector<string> result;
+    if (file.is_open()) {
+        while (getline(file, line))
+            result.push_back(line);
+    }
+    file.close();
+    return result;
+}
+
+int main() {
+    vector<string> names = readData(NAMES);
+    vector<string> handles = readData(HANDLES);
+
+    string json = "[\n";
+    for (int i = 0; i < min(names.size(), handles.size()); i++)
+        json += "\t{\n\t\"name\": \"" + names[i] + "\",\n" + "\t\"handle\": \"" + handles[i] + "\"\n\t},\n";
+
+    json.pop_back();
+    json.pop_back();
+    json += "\n]";
+    ofstream file(OUTPUT);
+    file << json;
+    cout << "Done | Data has been parsed to: " << OUTPUT << endl;
+    return 0;
+}
